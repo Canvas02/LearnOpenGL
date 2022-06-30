604f10b37bf30f52b189b3ceb25eb1c8bc286878
Canavas02 <Canvas02@protonmail.com>
2022-06-30 22:56:50 +0300

    Fixed macro error

--- LearnOpenGL/include/platfrom.h
+++ LearnOpenGL/include/platfrom.h
@@ -1,21 +1,13 @@
 #pragma once
 
-#if defined(_WIN32) && !defined(UNIX)
-#	define WINDOWS
-#elif !defined(_WIN32) && defined(UNIX)
-#	define POSIX
-#endif
-
 #if defined(__GNUC__)
-#	define GCC
+#define GCC
 #elif defined(_MSC_VER)
-#	define MSVC
-#endif // defined(__GNUC__)
-
+#define MSVC
+#endif
 
 #if defined(WINDOWS)
-#	define DLL_EXPORT __declspec(dllexport)
+#define DLL_EXPORT __declspec(dllexport)
 #elif defined(POSIX)
-#	define DLL_EXPORT __attribute__((visibility("default")))
+#define DLL_EXPORT __attribute__((visibility("default")))
 #endif
- 
--- LearnOpenGL/src/main.cxx
+++ LearnOpenGL/src/main.cxx
@@ -11,19 +11,18 @@
 #include "file.h"
 #include "shader_util.h"
 
-extern "C" 
+extern "C"
 {
-	DLL_EXPORT unsigned long	NvOptimusEnablement = 0x00000001;
-	DLL_EXPORT int				AmdPowerXpressRequestHighPerformance = 1;
+	DLL_EXPORT unsigned long NvOptimusEnablement = 0x00000001;
+	DLL_EXPORT int AmdPowerXpressRequestHighPerformance = 1;
 }
 
 constexpr int32_t WIDTH = 800, HEIGHT = 600;
 
 constexpr std::array<float_t, 9> vertices{
-	 0.5f, -0.5f, 0.0f,
+	0.5f, -0.5f, 0.0f,
 	-0.5f, -0.5f, 0.0f,
-	 0.0f,  0.5f, 0.0f
-};
+	0.0f, 0.5f, 0.0f};
 
 /*
 constexpr std::array<unsigned int, 6> indices{
@@ -34,30 +33,30 @@ constexpr std::array<unsigned int, 6> indices{
 
 int main()
 {
-    if (!glfwInit())
-    {
-        spdlog::error("Failed to init glfw");
-        glfwTerminate();
-        return -1;
-    }
+	if (!glfwInit())
+	{
+		spdlog::error("Failed to init glfw");
+		glfwTerminate();
+		return -1;
+	}
 
-    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
-    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 6);
-    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
-#   if !defined(NDEBUG)
-		glfwWindowHint(GLFW_OPENGL_DEBUG_CONTEXT, GL_TRUE);
-#   endif // !defined(NDEBUG)
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 6);
+	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+#if !defined(NDEBUG)
+	glfwWindowHint(GLFW_OPENGL_DEBUG_CONTEXT, GL_TRUE);
+#endif // !defined(NDEBUG)
 
-    auto window = glfwCreateWindow(WIDTH, HEIGHT, "LearnOpenGL", nullptr, nullptr);
-    if (!window)
-    {
-        spdlog::error("Failed to create glfw window");
-        glfwTerminate();
-        return -1;
-    }
-    glfwMakeContextCurrent(window);
-    glfwSwapInterval(1);    // V-sync
-    
+	auto window = glfwCreateWindow(WIDTH, HEIGHT, "LearnOpenGL", nullptr, nullptr);
+	if (!window)
+	{
+		spdlog::error("Failed to create glfw window");
+		glfwTerminate();
+		return -1;
+	}
+	glfwMakeContextCurrent(window);
+	glfwSwapInterval(1); // V-sync
+
 	if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
 	{
 		spdlog::error("Failed to load opengl functions");
@@ -65,13 +64,13 @@ int main()
 		return 1;
 	}
 
-    glfwSetKeyCallback(window, key_callback);
-    glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
+	glfwSetKeyCallback(window, key_callback);
+	glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
 
-    spdlog::info("{}", glGetString(GL_RENDERER));
+	spdlog::info("{}", glGetString(GL_RENDERER));
 	spdlog::info("Version: {}", glGetString(GL_VERSION));
 
-    // Init debug
+	// Init debug
 	int32_t flags = 0;
 	glGetIntegerv(GL_CONTEXT_FLAGS, &flags);
 	if (flags & GL_CONTEXT_FLAG_DEBUG_BIT)
@@ -86,7 +85,7 @@ int main()
 	{
 		puts("Debug output disabled\n\n");
 	}
-	
+
 	uint32_t vbo, vao;
 	glGenVertexArrays(1, &vao);
 	glGenBuffers(1, &vbo);
@@ -100,8 +99,8 @@ int main()
 
 	const auto vert_src = read_file("res/shaders/basic.vert.glsl");
 	const auto frag_src = read_file("res/shaders/basic.frag.glsl");
-	//const auto y_frag_src = read_file("res/shaders/yellow.frag.glsl");
-	//const auto b_frag_src = read_file("res/shaders/blue.frag.glsl");
+	// const auto y_frag_src = read_file("res/shaders/yellow.frag.glsl");
+	// const auto b_frag_src = read_file("res/shaders/blue.frag.glsl");
 
 	const auto program = make_program(vert_src.c_str(), frag_src.c_str());
 	glUseProgram(program);
@@ -131,5 +130,5 @@ int main()
 	glDeleteProgram(program);
 
 	glfwTerminate();
-    return 0;
+	return 0;
 }
