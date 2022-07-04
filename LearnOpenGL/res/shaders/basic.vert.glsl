262c796bf6280381e405dabb2a328db62f640c59
Canvas02 <Canvas02@protonmail.com>
2022-07-04 22:10:17 +0300

    color = position!!

--- LearnOpenGL/res/shaders/basic.vert.glsl
+++ LearnOpenGL/res/shaders/basic.vert.glsl
@@ -5,10 +5,8 @@ layout (location = 1) in vec3 aColor;
 
 out vec3 fColor;
 
-uniform float offset;
-
 void main()
 {
-	gl_Position = vec4(aPos.x + offset, aPos.y, aPos.z, 1.0);
-	fColor = aColor;
+	gl_Position = vec4(aPos, 1.0);
+	fColor = aPos;
 }
--- LearnOpenGL/src/main.cxx
+++ LearnOpenGL/src/main.cxx
@@ -110,19 +110,10 @@ int main()
 		ShaderProgram program(vert_src.c_str(), frag_src.c_str());
 		program.use();
 
-		std::vector<float_t> offset{ 1 };
-
 		while (!glfwWindowShouldClose(window))
 		{
 			glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
 			glClear(GL_COLOR_BUFFER_BIT);
-			
-			program.setUniform("offset", offset);
-			if (offset[0] > -1)
-				offset[0] -= 0.08f;
-			else
-				offset[0] = 1;
-
 
 			glBindVertexArray(vao);
 			glDrawArrays(GL_TRIANGLES, 0, 3);
