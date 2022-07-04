5d71744f922d9d498e4dc86158e6cfc66321dc5b
Canvas02 <Canvas02@protonmail.com>
2022-07-05 00:02:29 +0300

    Crate!! (with the power of textures)

--- LearnOpenGL/res/shaders/basic.frag.glsl
+++ LearnOpenGL/res/shaders/basic.frag.glsl
@@ -1,9 +1,13 @@
 #version 430 core
 
 in vec3 fColor;
+in vec2 fTexCoord;
+
 out vec4 color;
+
+uniform sampler2D oTexture;
 
 void main()
 {
-        color = vec4(fColor, 1.0);
+	color = texture(oTexture, fTexCoord);
 }
--- LearnOpenGL/src/main.cxx
+++ LearnOpenGL/src/main.cxx
@@ -5,6 +5,8 @@
 #include <GLFW/glfw3.h>
 #include <spdlog/spdlog.h>
 #include <fmt/core.h>
+#define STB_IMAGE_IMPLEMENTATION
+#include <stb_image.h>
 
 #include "platfrom.h"
 #include "callbacks.h"
@@ -19,18 +21,18 @@ extern "C"
 
 constexpr int32_t WIDTH = 800, HEIGHT = 600;
 
-constexpr std::array<float_t, 6 * 3> vertices{
-	0.5f, -0.5f, 0.0f, 1.0f, 0.0f, 0.0f,
-	-0.5f, -0.5f, 0.0f, 0.0f, 1.0f, 0.0f,
-	0.0f, 0.5f, 0.0f, 0.0f, 0.0f, 1.0f
+constexpr std::array<float_t, 8 * 4> vertices{
+//	 [0] aPos				[1] aColor			[2] aTexCoord
+	 0.5f,  0.5f, 0.0f,		1.0f, 0.0f, 0.0f,	1.0f, 1.0f,
+	 0.5f, -0.5f, 0.0f,		0.0f, 1.0f, 0.0f,	1.0f, 0.0f,
+	-0.5f, -0.5f, 0.0f,		0.0f, 0.0f, 1.0f,	0.0f, 0.0f,
+	-0.5f,  0.5f, 0.0f,		1.0f, 1.0f, 0.0f,	0.0f, 1.0f
 };
 
-/*
-constexpr std::array<unsigned int, 6> indices{
+constexpr std::array<uint32_t, 6> indices{
 	0, 1, 2,
 	0, 3, 2
 };
-*/
 
 int main()
 {
@@ -38,7 +40,7 @@ int main()
 	{
 		spdlog::error("Failed to init glfw");
 		glfwTerminate();
-		return -1;
+		exit(EXIT_FAILURE);
 	}
 
 	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
@@ -53,7 +55,7 @@ int main()
 	{
 		spdlog::error("Failed to create glfw window");
 		glfwTerminate();
-		return -1;
+		exit(EXIT_FAILURE);
 	}
 	glfwMakeContextCurrent(window);
 	glfwSwapInterval(1); // V-sync
@@ -62,7 +64,7 @@ int main()
 	{
 		spdlog::error("Failed to load opengl functions");
 		glfwTerminate();
-		return 1;
+		exit(EXIT_FAILURE);
 	}
 
 	glfwSetKeyCallback(window, key_callback);
@@ -87,19 +89,50 @@ int main()
 		puts("Debug output disabled\n\n");
 	}
 
-	uint32_t vbo, vao;
+	// Texture options
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_MIRRORED_REPEAT);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_MIRRORED_REPEAT);
+
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST_MIPMAP_LINEAR);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+
+	// Load image
+	int32_t width, height, nrChannels;
+	auto data = stbi_load("res/textures/wood_container.jpg", &width, &height, &nrChannels, 0);
+	if (!data)
+	{
+		spdlog::error("Failed to load image");
+		glfwTerminate();
+		exit(EXIT_FAILURE);
+	}
+
+	uint32_t texture;
+	glGenTextures(1, &texture);
+	glBindTexture(GL_TEXTURE_2D, texture);
+	glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width, height, 0, GL_RGB, GL_UNSIGNED_BYTE, data);
+	glGenerateMipmap(GL_TEXTURE_2D);
+
+	stbi_image_free(data);
+
+	uint32_t vbo, vao, ibo;
 	glGenVertexArrays(1, &vao);
 	glGenBuffers(1, &vbo);
+	glGenBuffers(1, &ibo);
 
 	glBindVertexArray(vao);
 	glBindBuffer(GL_ARRAY_BUFFER, vbo);
+	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, ibo);
 	glBufferStorage(GL_ARRAY_BUFFER, vertices.size() * sizeof(float_t), &vertices[0], 0);
+	glBufferStorage(GL_ELEMENT_ARRAY_BUFFER, indices.size() * sizeof(uint32_t), &indices[0], 0);
 
-    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, sizeof(float_t) * 6, (void*)0);
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, sizeof(float_t) * 8, (void*)0);
 	glEnableVertexAttribArray(0);
 
-    glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, sizeof(float_t) * 6, (void*)(sizeof(float) * 3));
+    glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, sizeof(float_t) * 8, (void*)(sizeof(float) * 3));
     glEnableVertexAttribArray(1);
+
+    glVertexAttribPointer(2, 2, GL_FLOAT, GL_FALSE, sizeof(float_t) * 8, (void*)(sizeof(float) * 6));
+    glEnableVertexAttribArray(2);
 
 	const auto vert_src = read_file("res/shaders/basic.vert.glsl");
 	const auto frag_src = read_file("res/shaders/basic.frag.glsl");
@@ -115,8 +148,9 @@ int main()
 			glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
 			glClear(GL_COLOR_BUFFER_BIT);
 
+			glBindTexture(GL_TEXTURE_2D, texture);
 			glBindVertexArray(vao);
-			glDrawArrays(GL_TRIANGLES, 0, 3);
+			glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
 
 			glfwPollEvents();
 			glfwSwapBuffers(window);
@@ -128,6 +162,7 @@ int main()
 	catch (gl_error& ecx)
 	{
 		spdlog::error("Opengl: {}", ecx.what());
+		glfwTerminate();
 		exit(EXIT_FAILURE);
 	}
 
