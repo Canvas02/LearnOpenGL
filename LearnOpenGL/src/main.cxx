ccea3e2060dc38aa6a5d221b16e052f2bf9c8253
Canavas02 <Canvas02@protonmail.com>
2022-07-16 21:07:52 +0300

    Now its differentgit add .!

--- LearnOpenGL/src/main.cxx
+++ LearnOpenGL/src/main.cxx
@@ -208,8 +208,8 @@ int main()
 			glBindTexture(GL_TEXTURE_2D, texture1);
 
 			glm::mat4 trans = glm::mat4(1.0f);
-			trans = glm::translate(trans, glm::vec3(0.5f, -0.5f, 0.0f));
 			trans = glm::rotate(trans, (float)glfwGetTime(), glm::vec3(0.0f, 0.0f, 1.0f));
+			trans = glm::translate(trans, glm::vec3(0.5f, -0.5f, 0.0f));
 
 			program.setUniform("uTextureMix", uTextureMix);
 			glUniformMatrix4fv(transformLoc, 1, GL_FALSE, glm::value_ptr(trans));
