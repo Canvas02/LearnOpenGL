53a820e65c79380251a6f0d185689c8471e165a5
Canavas02 <Canvas02@protonmail.com>
2022-07-18 21:04:37 +0300

    Moooore!

--- LearnOpenGL/src/main.cxx
+++ LearnOpenGL/src/main.cxx
@@ -233,8 +233,10 @@ int main()
 		auto proj = glm::mat4(1.0f);
 
 		proj = glm::perspective(glm::radians(45.0f), (float_t)WIDTH / (float_t)HEIGHT, 0.1f, 100.0f);
+		view = glm::translate(view, glm::vec3(0.0f, 0.0f, -3.0f));
 
 		glUniformMatrix4fv(uProj, 1, GL_FALSE, glm::value_ptr(proj));
+		glUniformMatrix4fv(uView, 1, GL_FALSE, glm::value_ptr(view));
 
 		while (!glfwWindowShouldClose(window))
 		{
@@ -246,22 +248,22 @@ int main()
 			glActiveTexture(GL_TEXTURE1);
 			glBindTexture(GL_TEXTURE_2D, texture1);
 			
-			// view = glm::mat4(1.0f);
-			// view = glm::translate(view, glm::vec3(0.0f, 0.0f, -3.0f));
+			// model = glm::mat4(1.0f);
+			// model = glm::rotate(model, glm::radians(50.0f) * (float_t)glfwGetTime(), glm::vec3(0.5f, 1.0f, 0.0f));
+			// glUniformMatrix4fv(uModel, 1, GL_FALSE, glm::value_ptr(model));
 
-			model = glm::mat4(1.0f);
-			model = glm::rotate(model, glm::radians(50.0f) * (float_t)glfwGetTime(), glm::vec3(0.5f, 1.0f, 0.0f));
+			glBindVertexArray(vao);
+			for (uint32_t i = 0; i < 10; i++)
+			{
+				model = glm::mat4(1.0f);
+				model = glm::translate(model, cubePositions[i]);
+				float_t angle = 20.0f * i;
+				model = glm::rotate(model, glm::radians(angle), glm::vec3(1.0f, 0.3f, 0.5f));
+				glUniformMatrix4fv(uModel, 1, GL_FALSE, glm::value_ptr(model));
 
-			glUniformMatrix4fv(uView, 1, GL_FALSE, glm::value_ptr(view));
-			glUniformMatrix4fv(uModel, 1, GL_FALSE, glm::value_ptr(model));
+				glDrawArrays(GL_TRIANGLES, 0, 36);
+			}
 
-			glDrawArrays(GL_TRIANGLES, 0, 36);
-
-			// glBindVertexArray(vao);
-			// for (uint32_t i = 0; i < 10; i++)
-			// {
-			// 	model = glm::mat4(1.0f);
-			// }
 
 			glfwPollEvents();
 			glfwSwapBuffers(window);
